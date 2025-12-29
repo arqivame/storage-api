e3886f38dbb78da37c9cff01400048624877f889
jhonatapers <jhonatapers@gmail.com>
2025-12-29 17:21:16 -0300

    wip: pre-implementa metodos 'append' que serão utilizados no merge final do arquivo

--- infrastructure/src/main/java/com/arqivame/storage/infrastructure/commons/FileSystemUtils.java
+++ infrastructure/src/main/java/com/arqivame/storage/infrastructure/commons/FileSystemUtils.java
@@ -2,10 +2,12 @@ package com.arqivame.storage.infrastructure.commons;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.FileChannel;
 import java.nio.file.CopyOption;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 import com.arqivame.storage.domain.exception.InternalErrorException;
 
@@ -43,6 +45,48 @@ public final class FileSystemUtils {
         } catch (IOException e) {
             throw InternalErrorException.with("Failed to delete file: " + filePath.toString(), e);
         }
+    }
+
+    public static void append(final Path targetFile, final SequentialIterator<Path> sourceFiles) {
+
+        try (final FileChannel outputChannel = FileChannel.open(
+                targetFile,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE)) {
+
+            while (sourceFiles.hasNext()) {
+                try (final FileChannel inputChannel = FileChannel.open(sourceFiles.next(), StandardOpenOption.READ)) {
+
+                    final Long startPosition = outputChannel.position();// TODO ver se isso ta certo
+                    final Long endPosition = append(startPosition, outputChannel, inputChannel);
+                    outputChannel.position(endPosition);
+
+                }
+            }
+
+        } catch (Exception e) {
+            throw InternalErrorException.with("Failed to append file into " + targetFile.toString(), e);
+        }
+
+    }
+
+    private static Long append(
+            final Long startPosition,
+            final FileChannel targetChannel,
+            final FileChannel sourceChannel) throws Exception {
+
+        targetChannel.position(startPosition);
+
+        Long inputSize = sourceChannel.size();
+        Long trasferredBytes = 0L;
+        while (trasferredBytes < inputSize) {
+            trasferredBytes += sourceChannel.transferTo(trasferredBytes, inputSize - trasferredBytes, targetChannel);
+        }
+
+        final Long endPosition = targetChannel.position() + inputSize;
+        targetChannel.position(endPosition);
+
+        return endPosition;
     }
 
 }
