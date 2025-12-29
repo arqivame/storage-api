9f393af48de60abbf01736446c97db8638d77879
jhonatapers <jhonatapers@gmail.com>
2025-12-29 08:17:23 -0300

    fix: usa ':' para separar entidades por hierarquia.

--- domain/src/main/java/com/arqivame/storage/domain/file/event/FileUploadSessionMarkedForDeletionEvent.java
+++ domain/src/main/java/com/arqivame/storage/domain/file/event/FileUploadSessionMarkedForDeletionEvent.java
@@ -16,7 +16,7 @@ import com.arqivame.storage.domain.file.UploadSession;
 
 public class FileUploadSessionMarkedForDeletionEvent extends Event<FileUploadSessionMarkedForDeletionEvent.Data> {
 
-    private static final String ENTITY = "file.upload-session";
+    private static final String ENTITY = "file:upload-session";
     private static final String ACTION = "marked-for-deletion";
     private static final String VERSION = "0.0.1";
 
